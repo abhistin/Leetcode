6fc8724219389c42211b7dc0eb36df1531313fe6
Abhishek Bhardwaj <abhishek.sharmar112@gmail.com>
2024-04-15 23:33:13 +0530

    Add one comment

--- Arrays/169. Majority Element.playground/Contents.swift
+++ Arrays/169. Majority Element.playground/Contents.swift
@@ -1,16 +1,18 @@
 class Solution {
+
+    //This question uses Moore's Voting Algorithm.
+    
     static func majorityElement(_ nums: [Int]) -> Int {
         var majorityElement = nums.first!
         var counter = 0
         for num in nums {
-            if num == majorityElement {
+            if counter == 0 {
+                majorityElement = num
+            }
+            if majorityElement == num {
                 counter += 1
             } else {
                 counter -= 1
-            }
-            if counter == 0 {
-                majorityElement = num
-                counter = 1
             }
         }
         return majorityElement
